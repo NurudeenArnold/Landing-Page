d656e3bd55b381024101e5c6eea75a80cf3fb3c4
Nurudeen <nurudeenarnold@gmail.com>
2024-03-05 14:54:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let headerBackgrounds = document.querySelectorAll(".background");
+
+let imageIndex = 0;
+
+function changeBackground() {
+    headerBackgrounds[imageIndex].classList.remove("showing");
+
+    imageIndex++;
+
+    if(imageIndex >= headerBackgrounds.length) {
+        imageIndex = 0;
+    }
+
+    headerBackgrounds[imageIndex].classList.add("showing");
+}
+
+setInterval(changeBackground, 5000);
+
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", function(e) {
+    cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX
+    +"px; top: " + e.clientY + "px;";
+});
