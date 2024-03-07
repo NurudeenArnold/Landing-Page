c718b2da985a014202482d9d4de81790480fd080
Nurudeen <nurudeenarnold@gmail.com>
2024-03-07 16:01:14 +0200

    .

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let headerBackgrounds = document.querySelectorAll(".background");
-
 let imageIndex = 0;
 
 function changeBackground() {
     headerBackgrounds[imageIndex].classList.remove("showing");
-
     imageIndex++;
-
     if (imageIndex >= headerBackgrounds.length) {
         imageIndex = 0;
     }
-
     headerBackgrounds[imageIndex].classList.add("showing");
 }
 setInterval(changeBackground, 5000);
