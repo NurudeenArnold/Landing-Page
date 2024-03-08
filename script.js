380b5cb2be61aae4b93d2618a4402e2f75229ff6
Nurudeen <nurudeenarnold@gmail.com>
2024-03-08 12:17:52 +0200

    Submit Functionality

--- script.js
+++ script.js
@@ -16,4 +16,23 @@ var cursor2 = document.querySelector(".cursor2");
 document.addEventListener("mousemove", function(e) {
     cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX
     +"px; top: " + e.clientY + "px;";
+}); 
+
+var submitForm = document.getElementById("submitForm");
+submitForm.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    var email = document.getElementById("email").value;
+    console.log(email);
+
+    Swal.fire({
+        title: "Thank you for signing up for the Valorant beta!",
+        text: "Your interest in joining our community is truly appreciated. We'll keep you updated on any developments and look forward to having you join us in the game soon. Stay tuned for more updates!",
+        color: "#ffffff",
+        icon: "success",
+        confirmButtonColor: "#ff3333",
+        iconColor: "#ff3333",
+        confirmButtonText: "Okay"
+      });
+    document.getElementById("email").value = "";
 });
